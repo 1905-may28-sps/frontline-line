60cf2e38893b97207a9618e773318556dd5818fb
DHutson97 <blankempty97@gmail.com>
2019-07-24 01:00:11 -0400

    creating Ds new branch

---
+++ src/app/model/post.ts
@@ -0,0 +1,9 @@
+import { User } from './user';
+
+export class Post {
+    user:User;
+    body: string;
+    image:string;
+    timestamp:string;
+    
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  postAppUrl: 'http://localhost:8081/Project2'
+};
