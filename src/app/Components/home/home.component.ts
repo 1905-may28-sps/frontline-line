3a529521edcd0769dca3e0be4d88fdce3f0e81e5
kemiolatunji10 <orolatun@gmail.com>
2019-07-25 20:53:17 -0400

    search work

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -3,6 +3,9 @@ import { UserService } from '../../service/user.service';
 import { User } from 'src/app/model/user.model';
 import { PostService } from 'src/app/service/post.service';
 import { Post } from 'src/app/model/post';
+import {MessageService} from 'src/app/service/message.service';
+import {Message} from 'src/app/model/message.model';
+
 
 @Component({
   selector: 'app-home',
@@ -14,15 +17,18 @@ export class HomeComponent implements OnInit {
   posts: Post[]=[];
   post: Post=new Post();
 
+  searchText: string = '';
+
   imageSrc = ''
 
-  constructor(private userService: UserService, private postService: PostService) { 
+  constructor(private userService: UserService, private postService: PostService, private messageService: MessageService) { 
     console.log('in user service constructor')
   }
 
   ngOnInit() {
     this.getUsers();
     this.getPosts();
+    //this.getMessages();
   }
   getUsers(){
     this.userService.getUsers().subscribe(
@@ -68,4 +74,47 @@ export class HomeComponent implements OnInit {
       }
     )
   }
-}
+
+  filterPosts(){
+    this.posts = this.posts.filter(it => {
+      console.log('In filter post');
+      this.searchText = this.searchText.toLowerCase();
+      let test =  it.body.toLowerCase().includes(this.searchText);
+      console.log(it.body + test);
+      console.log(this.searchText);
+      return test ;
+    });
+  }
+
+//   getMessages (){
+//   this.messageService.getMessages().subscribe(
+//     resp => {
+//       if(resp != null){
+//         console.log(resp);
+//         this.messages = resp;
+//       }
+//       else{
+//         console.log('Error loading message, null value sent back')
+//       }
+//     },
+//     error => console.log('something unexpected happened')
+//   );
+// }
+
+// addMessage(){
+//     console.log("In adding a message");
+//     console.log(this.message);
+//     this.messageService.addMessage(this.message).subscribe(
+//       resp=>{
+//         console.log(resp);
+//         this.messages.push(resp);
+//         this.message=new Message();
+//       },
+//       error=>{
+//         console.log('failed at post');
+//       }
+
+
+//     )
+//   }
+  }
